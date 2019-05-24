a717ef46f0bf29e6d70131331436786fd8531db3
Dolwin <dolwinf@gmail.com>
2019-05-24 20:27:15 +1000

    Added Home component functionality

--- screens/Home.js
+++ screens/Home.js
@@ -1,13 +1,39 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 class Home extends React.Component {
+
+    state = {
+        counter: 0
+    }
+
+    add = () => {
+        num = this.state.counter + 1
+        this.setState({ counter: num})
+    }
+
+    
+    subtract = () => {
+        num = this.state.counter - 1
+        this.setState({ counter: num})
+    }
+
+    
+    reset = () => {
+        this.setState({ counter: 0})
+    }
   render() {
     return (
       <View style={styles.container}>
-        <Text>Home Component</Text>
+      <Text>Sumed up to: {this.state.counter}</Text>
+      <Button title="Add" onPress={()=> this.add()} />
+        <Button title="Subtract" onPress={()=> this.subtract()} />
+        <Button title="Reset" onPress={()=> this.reset()} />
       </View>
-    );
+        
+        
+     
+    );  
   }
 }
 
