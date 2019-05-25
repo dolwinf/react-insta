4bd6d699d82db12bbd0ce14a5c74cdafce1858f2
Dolwin <dolwinf@gmail.com>
2019-05-25 18:08:47 +1000

    Connected Profile component to store with add subtract and reset functionality

--- screens/Profile.js
+++ screens/Profile.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { connect } from 'react-redux';
 
 class Profile extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Profile Component</Text>
+        <Text>Profile Component also has state {this.props.count}</Text>
+        <Button title="Add" onPress={()=> this.props.add()} />
+        <Button title="Subtract" onPress={()=> this.props.subtract()} />
+        <Button title="Reset" onPress={()=> this.props.reset()} />
       </View>
     );
   }
@@ -20,4 +24,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default Profile;
+export default connect(mapStateToProps, mapDispatchToProps)(Profile)
